7ed3e028d67895c0092e01cd78513f132e174a0f
Lucas Cabral <lcc2@cesar.org.br>
2019-03-28 14:07:52 -0300

    Clean unnecessary code

--- app/src/main/java/br/org/cesar/knot_setup_app/model/BluetoothDevice.java
+++ app/src/main/java/br/org/cesar/knot_setup_app/model/BluetoothDevice.java
@@ -2,23 +2,19 @@ package br.org.cesar.knot_setup_app.model;
 
 import android.support.annotation.NonNull;
 
+
 public class BluetoothDevice implements Comparable<BluetoothDevice> {
 
     private android.bluetooth.BluetoothDevice device;
     private int rssi;
     private String pin;
 
-    //TODO: Check what fields are necessary in here
 
     public BluetoothDevice(android.bluetooth.BluetoothDevice device, int rssi) {
         this.device = device;
         this.rssi = rssi;
     }
 
-    public BluetoothDevice(android.bluetooth.BluetoothDevice device, int rssi, byte[] sharedKey, byte[] mainKey, byte[] guestPass) {
-        this.device = device;
-        this.rssi = rssi;
-    }
 
     public android.bluetooth.BluetoothDevice getDevice() {
         return device;
@@ -27,6 +23,7 @@ public class BluetoothDevice implements Comparable<BluetoothDevice> {
     public int getRssi() {
         return rssi;
     }
+
 
     public String getPin() {
         return pin;
