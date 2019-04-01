e1e46b34a287f43e450e07130da3697792f28682
Lucas Cabral <lcc2@cesar.org.br>
2019-04-01 19:44:52 -0300

    Add gateway adapter class

---
+++ app/src/main/java/br/org/cesar/knot_setup_app/views/adapter/gatewayAdapter.java
@@ -0,0 +1,55 @@
+package br.org.cesar.knot_setup_app.views.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+import br.org.cesar.knot_setup_app.R;
+
+public class gatewayAdapter extends ArrayAdapter<String> {
+    private List<String> deviceList;
+
+
+    public gatewayAdapter(@NonNull Context context, int resource, @NonNull List<String>
+            objects) {
+        super(context, resource, objects);
+
+        deviceList = objects;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        final ViewHolder holder;
+
+        if (convertView == null) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_gateway,
+                    parent, false);
+            holder.deviceName = convertView.findViewById(R.id.device_name);
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        String device = deviceList.get(position);
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+
+        private TextView deviceName;
+        private ImageView deviceImage;
+
+    }
+}
