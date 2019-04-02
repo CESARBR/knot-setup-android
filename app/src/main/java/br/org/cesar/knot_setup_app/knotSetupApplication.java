faf2d4584725e257eed550c59bf0409f2385b460
Lucas Cabral <lcc2@cesar.org.br>
2019-04-02 15:02:47 -0300

    Add daemon to share resources

---
+++ app/src/main/java/br/org/cesar/knot_setup_app/knotSetupApplication.java
@@ -0,0 +1,28 @@
+package br.org.cesar.knot_setup_app;
+
+import android.app.Application;
+
+import br.org.cesar.knot_setup_app.model.BluetoothDevice;
+import br.org.cesar.knot_setup_app.wrapper.BluetoothWrapper;
+
+// This class is used for sharing bluetooth between activities
+public class knotSetupApplication extends Application {
+
+    private static BluetoothWrapper bluetoothWrapper;
+    private static BluetoothDevice bluetoothDevice;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        bluetoothWrapper = new BluetoothWrapper();
+        bluetoothDevice = new BluetoothDevice(null,0);
+    }
+
+    public static BluetoothWrapper getBluetoothWrapper() {
+        return bluetoothWrapper;
+    }
+
+    public static void setBluetoothDevice(BluetoothDevice device){bluetoothDevice = device;}
+
+    public static BluetoothDevice getBluetoothDevice(){return bluetoothDevice;}
+}
