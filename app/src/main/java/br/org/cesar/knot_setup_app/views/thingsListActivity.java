7c997bf8434fe6a524f51b68cddb3dd0a32a5f70
Lucas Cabral <lcc2@cesar.org.br>
2019-04-03 11:14:06 -0300

    Add floating_action_button to thingsListActivity

--- app/src/main/java/br/org/cesar/knot_setup_app/views/thingsListActivity.java
+++ app/src/main/java/br/org/cesar/knot_setup_app/views/thingsListActivity.java
@@ -1,7 +1,10 @@
 package br.org.cesar.knot_setup_app.views;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import br.org.cesar.knot_setup_app.R;
 
@@ -11,5 +14,20 @@ public class thingsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_things_list);
+
+
+        FloatingActionButton addButton = findViewById(R.id.add_thing);
+
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: Pass something that sinalizes the UUID i want to filter by
+                Intent i = new Intent(thingsListActivity.this,scanDeviceActivity.class );
+                startActivity(i);
+            }
+        });
+
+
     }
 }
