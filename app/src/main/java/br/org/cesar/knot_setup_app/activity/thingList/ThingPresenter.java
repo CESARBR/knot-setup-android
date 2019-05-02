ec756af1bce3289b84e44ef7edc5118dec175615
Lucas Cabral <lcc2@cesar.org.br>
2019-05-02 16:55:07 -0300

    Add list things using backend of webui
    Listing the THINGs based on the backend of the webui rather than from
    the internal storage of the android device.

--- app/src/main/java/br/org/cesar/knot_setup_app/activity/thingList/ThingPresenter.java
+++ app/src/main/java/br/org/cesar/knot_setup_app/activity/thingList/ThingPresenter.java
@@ -1,16 +1,27 @@
 package br.org.cesar.knot_setup_app.activity.thingList;
 
+import android.util.Log;
+
 import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import br.org.cesar.knot_setup_app.activity.gatewayList.GatewayContract;
 import br.org.cesar.knot_setup_app.activity.thingList.ThingContract.ViewModel;
 import br.org.cesar.knot_setup_app.activity.thingList.ThingContract.Presenter;
+import br.org.cesar.knot_setup_app.data.DataManager;
+import br.org.cesar.knot_setup_app.data.api.ApiService;
+import br.org.cesar.knot_setup_app.model.Gateway;
 import br.org.cesar.knot_setup_app.persistence.mysqlDatabase.DBHelper;
+import br.org.cesar.knot_setup_app.utils.Constants;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
 
 public class  ThingPresenter implements Presenter{
     private ViewModel mViewModel;
     private DBHelper dbHelper;
     private int gatewayID;
+    private static DataManager dataManager;
 
     ThingPresenter(ViewModel viewModel,DBHelper dbHelper,int gatewayID) {
         mViewModel = viewModel;
@@ -19,9 +30,31 @@ public class  ThingPresenter implements Presenter{
     }
 
     public void getDeviceList(){
-        this.mViewModel.callbackOnDeviceList(this.dbHelper.getAllGatewayThings());
+        dataManager.getInstance().getService().getDevices(Constants.Oauth)
+                .timeout(30, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleDeviceSuccess,
+                        this::onErrorHandler);
     }
 
+    private void handleDeviceSuccess(List<Gateway> gat){
+        if(!gat.isEmpty()) {
+            ArrayList<String> deviceList = new ArrayList<String>();
 
+            Log.d("DEV-LOG", "Entered");
+            for (Gateway gt : gat) {
+                deviceList.add(gt.getName());
+            }
+            this.mViewModel.callbackOnDeviceList(deviceList);
+        }
+        else{
+            Log.d("DEV-LOG","There are no devices avaiable.");
+        }
+    }
+
+    private void onErrorHandler(Throwable throwable){
+        Log.d("DEV-LOG", "onErrorHandler: " + throwable.getMessage());
+    }
 
 }
