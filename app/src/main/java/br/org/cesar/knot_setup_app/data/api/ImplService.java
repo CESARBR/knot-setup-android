b6edc1683d913391a44db8518b0e7cb0f7756ece
Lucas Cabral <lcc2@cesar.org.br>
2019-05-07 11:10:54 -0300

    Add login request using retrofit

--- app/src/main/java/br/org/cesar/knot_setup_app/data/api/ImplService.java
+++ app/src/main/java/br/org/cesar/knot_setup_app/data/api/ImplService.java
@@ -3,10 +3,14 @@ import java.util.List;
 
 import br.org.cesar.knot_setup_app.model.Gateway;
 import br.org.cesar.knot_setup_app.model.State;
+import br.org.cesar.knot_setup_app.model.User;
 import io.reactivex.Observable;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
 import retrofit2.http.Path;
 
 public interface ImplService {
@@ -17,5 +21,8 @@ public interface ImplService {
     @GET("api/devices")
     Observable<List<Gateway>> getDevices(@Header("Authorization") String auth);
 
+    @FormUrlEncoded
+    @POST("/api/auth")
+    Observable<User> login(@Field("email") String email, @Field("password") String password);
 
 }
