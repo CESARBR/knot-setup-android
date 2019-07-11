022618683cb6f6dfa6937e5e12f623ec5825f7ea
Lucas Cabral <lcc2@cesar.org.br>
2019-07-11 19:47:15 -0300

    Add splash screen to app startup
    Added a splash screen to show to the user while the app is loading.

---
+++ app/src/main/java/br/org/cesar/knot_setup_app/view/IntroActivity.java
@@ -0,0 +1,19 @@
+package br.org.cesar.knot_setup_app.view;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import br.org.cesar.knot_setup_app.view.GatewayActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent i = new Intent(IntroActivity.this, GatewayActivity.class );
+        startActivity(i);
+        finish();
+    }
+
+}
