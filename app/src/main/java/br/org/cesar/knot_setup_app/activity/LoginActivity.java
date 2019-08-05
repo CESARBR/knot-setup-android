9ee8cbbbf86368fc1815b954ee442b25c6b33d48
Lucas Cabral <lcc2@cesar.org.br>
2019-08-05 20:34:38 -0300

    Change login to use tabs
    Change login to load one tab that holds a fragment that does exactly
    the same as the former login activity. This approach was taken to
    prevent the use of different layouts (other than the header layout)
    in order to keep the consistency of the app views.

---
+++ app/src/main/java/br/org/cesar/knot_setup_app/activity/LoginActivity.java
@@ -0,0 +1,31 @@
+package br.org.cesar.knot_setup_app.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+
+import br.org.cesar.knot_setup_app.R;
+import br.org.cesar.knot_setup_app.fragment.login.LoginFragment;
+import br.org.cesar.knot_setup_app.model.Tab;
+import br.org.cesar.knot_setup_app.utils.Constants;
+
+public class LoginActivity extends BaseActivity {
+
+    private String gatewayName;
+    private ArrayList<Tab> tabs;
+
+    @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            tabs = new ArrayList<>();
+            gatewayName = getIntent().getStringExtra(Constants.GATEWAY_ID);
+
+            Tab tab = new Tab(gatewayName, new LoginFragment());
+            tabs.add(tab);
+
+            setupHeader(getString(R.string.login_header_title),tabs);
+    }
+
+}
