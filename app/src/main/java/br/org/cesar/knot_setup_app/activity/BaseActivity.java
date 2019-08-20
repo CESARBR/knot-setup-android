71bbdacebdade0370b4ca93342ee5af6c2f5ba26
Lucas Cabral <lucascostacabra@gmail.com>
2019-08-20 08:20:31 -0300

    Add gateway activity that holds two tabs
    Add gateway activty that holds two fragments. Also added a base activity
    since other activities (ThingActivity and so on) will need to use tabs.
    BaseActivity is used to  abstract the use of tabs.

---
+++ app/src/main/java/br/org/cesar/knot_setup_app/activity/BaseActivity.java
@@ -0,0 +1,48 @@
+package br.org.cesar.knot_setup_app.activity;
+
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+import br.org.cesar.knot_setup_app.R;
+import br.org.cesar.knot_setup_app.fragment.PageAdapter;
+import br.org.cesar.knot_setup_app.model.Tab;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    private PageAdapter pageAdapter;
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private TextView headerTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_header);
+
+        headerTitle = findViewById(R.id.list_title);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager());
+        viewPager = findViewById(R.id.container);
+
+        tabLayout = findViewById(R.id.tabs);
+    }
+
+    public void setupHeader(String headerTitle, ArrayList<Tab> tabs) {
+        this.headerTitle.setText(headerTitle);
+
+        for(Tab tab:tabs) {
+            pageAdapter.addList(tab.getFragment(),tab.getTabName());
+        }
+
+        viewPager.setAdapter(pageAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+}
--- app/src/main/java/br/org/cesar/knot_setup_app/activity/gatewayList/GatewayActivity.java
+++ app/src/main/java/br/org/cesar/knot_setup_app/activity/gatewayList/GatewayActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.net.nsd.NsdManager;
 import android.net.nsd.NsdServiceInfo;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -115,8 +114,8 @@ public class GatewayActivity extends AppCompatActivity implements  GatewayContra
 
     @Override
     public void setSearchingFeedback(int visibilty){
-        TextView lookingForGateway = findViewById(R.id.searchingForGatewayText);
-        ProgressBar progressBar = findViewById(R.id.progressBar3);
+        TextView lookingForGateway = findViewById(R.id.searching_for_gateway_text);
+        ProgressBar progressBar = findViewById(R.id.searching_gateway_spinner);
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
