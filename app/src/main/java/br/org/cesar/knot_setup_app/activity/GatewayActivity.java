e6ac4e4c778f2965f7a54bd1c68a7aa78d572e5e
Lucas Cabral <lucascostacabra@gmail.com>
2019-08-20 08:41:04 -0300

    Add scanFragment
    Add scanFragment that searches for gateways that are advertising
    somekind of service (the service is specified by the calling activity).

--- app/src/main/java/br/org/cesar/knot_setup_app/activity/GatewayActivity.java
+++ app/src/main/java/br/org/cesar/knot_setup_app/activity/GatewayActivity.java
@@ -8,18 +8,27 @@ import java.util.ArrayList;
 import br.org.cesar.knot_setup_app.R;
 import br.org.cesar.knot_setup_app.model.Tab;
 import br.org.cesar.knot_setup_app.fragment.gatewayConnected.GatewayConnectedFragment;
+import br.org.cesar.knot_setup_app.fragment.scan.ScanFragment;
+import br.org.cesar.knot_setup_app.utils.Constants;
 
 public class GatewayActivity extends BaseActivity {
 
     private ArrayList<Tab> tabs;
+
+    private ScanFragment scanWifiFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         tabs = new ArrayList<>();
 
+        //Setting the scan to look for a specific UUID
+        scanWifiFragment = new ScanFragment();
+        scanWifiFragment.setService(Constants.WIFI_CONFIGURATION_SERVICE_GATEWAY);
+
+
         Tab configureWiFiTab, gatewayConnectedTab;
-        configureWiFiTab = new Tab(getString(R.string.gateway_configure_WiFi), new Fragment());
+        configureWiFiTab = new Tab(getString(R.string.gateway_configure_WiFi), scanWifiFragment);
         gatewayConnectedTab = new Tab(getString(R.string.gateway_connected),
                 new GatewayConnectedFragment());
 
